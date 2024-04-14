c39c9e19a81d0b092f5122ae4e57b5c2c707deff
Kamily Grolli <kamily369@live.com>
2024-04-14 13:13:05 -0300

    deletarPessoa() e listarPessoas() criadas

--- gerenciadorDePessoas.js
+++ gerenciadorDePessoas.js
@@ -1,15 +1,4 @@
-/*
- Cadastro (Create): Você deve criar uma função cadastrarPessoa que recebe um objeto
-representando uma pessoa (com campos como "nome", "idade", "cidade", etc.) e o adiciona à
-lista de pessoas cadastradas.
- Atualização (Update): Você deve criar uma função atualizarPessoa que recebe um ID e um
-objeto com campos a serem atualizados. Ela deve encontrar a pessoa com o ID correspondente
-e atualizar os campos especificados.
- Deleção (Delete): Você deve criar uma função deletarPessoa que recebe um ID e remove a
-pessoa correspondente da lista.
- Listagem (Read): Você deve criar uma função listarPessoas que retorna a lista atual de pessoas
-cadastradas
-*/
+
 
 function gerarID() {
     return Math.floor(Math.random() * 1000)  //Gera IDs entre o e 999
@@ -29,31 +18,45 @@ function cadastrarPessoa(nome, idade, cidade) {
     pessoasCadastradas.push(pessoa)    
 };
 
-
-let pessoasCadastradas = [{ID: 832, nome: 'Kamily', idade: '20', cidade: 'na'}];
-
-
 //cadastrarPessoa(nome, idade, cidade);
-
 
 function atualizarPessoa(idAlterar, campoAlterar) {
     for (i in pessoasCadastradas) {
             if (idAlterar == pessoasCadastradas[i].ID) {
                 if (campoAlterar == "nome") {
-                    pessoasCadastradas[i].nome = prompt("Digite o novo nome: ")
+                    pessoasCadastradas[i].nome = prompt("Digite o novo nome: ");
                 } else if (campoAlterar == "idade") {
-                    pessoasCadastradas[i].idade = prompt("Digite a nova idade: ")
+                    pessoasCadastradas[i].idade = prompt("Digite a nova idade: ");
                 } else if (campoAlterar == "cidade") {
-                    pessoasCadastradas[i].cidade = prompt("Digite a nova cidade: ")
+                    pessoasCadastradas[i].cidade = prompt("Digite a nova cidade: ");
                 } else {
-                    alert("O campo para alterar é inválido!")
+                    alert("O campo para alterar é inválido!");
                 }
     
-            }
+            } 
+            alert("Não existe registor com esse ID!");
     }
 }
-
     
 //atualizarPessoa(832, "País");
 
-console.log(pessoasCadastradas);
+function deletarPessoa(idDeletar) {
+    for (i in pessoasCadastradas) {
+        if (idDeletar == pessoasCadastradas[i].ID) {
+            pessoasCadastradas.splice(i,1);
+        }
+    }
+}
+
+//deletarPessoa(1);
+
+function listarPessoas() {
+    console.log(pessoasCadastradas);
+}
+
+//listarPessoas()
+
+
+let pessoasCadastradas = [{ID: 832, nome: 'Kamily', idade: '20', cidade: 'na'},
+{ID: 1, nome: 'Laura', idade: '21', cidade: 'sy'}];
+
