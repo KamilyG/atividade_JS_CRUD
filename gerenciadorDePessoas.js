a1c2880de5582c7dd995f5af20ea23bf55763115
Kamily Grolli <kamily369@live.com>
2024-04-14 11:18:26 -0300

    first commit

---
+++ gerenciadorDePessoas.js
@@ -0,0 +1,14 @@
+/*
+Você foi contratado para criar um sistema de gerenciamento de pessoas em JavaScript. Você deve
+implementar quatro funções principais para realizar as operações CRUD:
+ Cadastro (Create): Você deve criar uma função cadastrarPessoa que recebe um objeto
+representando uma pessoa (com campos como "nome", "idade", "cidade", etc.) e o adiciona à
+lista de pessoas cadastradas.
+ Atualização (Update): Você deve criar uma função atualizarPessoa que recebe um ID e um
+objeto com campos a serem atualizados. Ela deve encontrar a pessoa com o ID correspondente
+e atualizar os campos especificados.
+ Deleção (Delete): Você deve criar uma função deletarPessoa que recebe um ID e remove a
+pessoa correspondente da lista.
+ Listagem (Read): Você deve criar uma função listarPessoas que retorna a lista atual de pessoas
+cadastradas
+*/
