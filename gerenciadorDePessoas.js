8d834944a9edfd753be8f16a509c82a108444c01
Kamily Grolli <kamily369@live.com>
2024-04-14 13:39:09 -0300

    Ajustes finais

--- gerenciadorDePessoas.js
+++ gerenciadorDePessoas.js
@@ -4,7 +4,7 @@ function gerarID() {
     return Math.floor(Math.random() * 1000)  //Gera IDs entre o e 999
 }
 
-function cadastrarPessoa(nome, idade, cidade) { 
+function cadastrarPessoa() { 
     nome = prompt("Digite o seu nome: ")
     idade = prompt("Digite a sua idade: ")
     cidade = prompt("Digite o nome da sua cidade: ")
@@ -34,7 +34,6 @@ function atualizarPessoa(idAlterar, campoAlterar) {
                 }
     
             } 
-            alert("Não existe registor com esse ID!");
     }
 }
     
@@ -56,7 +55,28 @@ function listarPessoas() {
 
 //listarPessoas()
 
-
 let pessoasCadastradas = [{ID: 832, nome: 'Kamily', idade: '20', cidade: 'na'},
 {ID: 1, nome: 'Laura', idade: '21', cidade: 'sy'}];
+
+while (true) {
+    let opcao = prompt("O que vocÊ quer fazer?\n 1-Cadastrar Pessoa\n 2-Atualizar Pessoa\n 3-Deletar Pessoa\n 4-Listar Pessoas\n 5-Sair");
+
+    if (opcao==1) {
+        cadastrarPessoa();
+    } else if (opcao==2) {
+        let id = prompt("Digite o ID a alterar:")
+        let campo = prompt("Digite o campo a alterar (nome, idade, cidade):")
+        atualizarPessoa(id,campo);
+    } else if (opcao==3) {
+        deletarPessoa(prompt("Digite o ID a deletar:"));
+    } else if (opcao==4) {
+        listarPessoas();
+    } else if (opcao==5) {
+        break
+    }
+    else {
+        alert("Entrada inválida!")
+    }
+}
+
 
