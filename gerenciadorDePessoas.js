b7dddbf134344e7d2d2453f62a6c985817529f2b
Kamily Grolli <kamily369@live.com>
2024-04-14 12:51:19 -0300

    cadastrarPessoa() e atualizarPessoa() criadas

--- gerenciadorDePessoas.js
+++ gerenciadorDePessoas.js
@@ -1,6 +1,4 @@
 /*
-Você foi contratado para criar um sistema de gerenciamento de pessoas em JavaScript. Você deve
-implementar quatro funções principais para realizar as operações CRUD:
  Cadastro (Create): Você deve criar uma função cadastrarPessoa que recebe um objeto
 representando uma pessoa (com campos como "nome", "idade", "cidade", etc.) e o adiciona à
 lista de pessoas cadastradas.
@@ -12,3 +10,50 @@ pessoa correspondente da lista.
  Listagem (Read): Você deve criar uma função listarPessoas que retorna a lista atual de pessoas
 cadastradas
 */
+
+function gerarID() {
+    return Math.floor(Math.random() * 1000)  //Gera IDs entre o e 999
+}
+
+function cadastrarPessoa(nome, idade, cidade) { 
+    nome = prompt("Digite o seu nome: ")
+    idade = prompt("Digite a sua idade: ")
+    cidade = prompt("Digite o nome da sua cidade: ")
+
+    const pessoa = {
+        ID: gerarID(),
+        nome: nome,
+        idade: idade,
+        cidade: cidade
+    };
+    pessoasCadastradas.push(pessoa)    
+};
+
+
+let pessoasCadastradas = [{ID: 832, nome: 'Kamily', idade: '20', cidade: 'na'}];
+
+
+//cadastrarPessoa(nome, idade, cidade);
+
+
+function atualizarPessoa(idAlterar, campoAlterar) {
+    for (i in pessoasCadastradas) {
+            if (idAlterar == pessoasCadastradas[i].ID) {
+                if (campoAlterar == "nome") {
+                    pessoasCadastradas[i].nome = prompt("Digite o novo nome: ")
+                } else if (campoAlterar == "idade") {
+                    pessoasCadastradas[i].idade = prompt("Digite a nova idade: ")
+                } else if (campoAlterar == "cidade") {
+                    pessoasCadastradas[i].cidade = prompt("Digite a nova cidade: ")
+                } else {
+                    alert("O campo para alterar é inválido!")
+                }
+    
+            }
+    }
+}
+
+    
+//atualizarPessoa(832, "País");
+
+console.log(pessoasCadastradas);
